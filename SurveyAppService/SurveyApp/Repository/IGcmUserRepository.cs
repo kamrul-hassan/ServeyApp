b18f54358e953c821dc124d36daad6660852827e
Kamrul Hassan <Kamrul.Hassan@bd.imshealth.com>
2016-03-31 15:25:44 +0600

    Add repository

---
+++ SurveyAppService/SurveyApp/Repository/IGcmUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using SurveyApp.Entity;
+using SurveyApp.Models;
+using System.Collections.Generic;
+
+namespace SurveyApp.Repository
+{
+    public interface IGcmUserRepository
+    {
+        GcmUserModel Save(GcmUser user);
+        List<GcmUserModel> GetGcmUsers();
+    }
+}
