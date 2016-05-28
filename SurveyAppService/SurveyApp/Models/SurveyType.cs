ed678baa81a61255de9e6bb873202f598075625b
Kamrul Hassan <kumubd@gmail.com>
2016-05-28 18:34:08 +0600

    Add file

---
+++ SurveyAppService/SurveyApp/Models/SurveyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SurveyApp.Models
+{
+    public class SurveyType
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Limit { get; set; }
+    }
+}
